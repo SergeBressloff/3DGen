4e933854504a67c468531379ae2ac10831d14b8e
Serge <151643754+SergeBressloff@users.noreply.github.com>
2025-07-15 14:11:53 +0100

    tidied up audio_recorder

--- audio_recorder.py
+++ audio_recorder.py
@@ -4,27 +4,6 @@ import wave
 import os
 from utils import resource_path
 
-def record_audio(filename=None, duration=5, samplerate=16000):
-    if filename is None:
-        filename = resource_path("audio/recording.wav")
-
-    audio_dir = os.path.dirname(filename)
-    os.makedirs(audio_dir, exist_ok=True)
-
-    print("Recording...")
-    audio = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1, dtype='int16')
-    sd.wait()
-
-    with wave.open(filename, 'w') as wf:
-        wf.setnchannels(1)
-        wf.setsampwidth(2)
-        wf.setframerate(samplerate)
-        wf.writeframes(audio.tobytes())
-        
-    print("Saved:", filename)
-
-
-
 class AudioRecorder:
     def __init__(self, samplerate=16000):
         self.samplerate = samplerate
